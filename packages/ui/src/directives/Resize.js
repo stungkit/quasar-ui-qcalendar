0f8466ff221645d1087e9863f321dc9e29fff0ad
Jeff <galbraith64@gmail.com>
2025-01-26 12:49:01 -0700

    fix(ui): Resize directive - check for "window"

--- packages/ui/src/directives/Resize.js
+++ packages/ui/src/directives/Resize.js
@@ -1,27 +1,32 @@
+/* global window */
 export default {
   name: 'Resize',
 
   mounted(el, { modifiers, value }) {
     if (!value) return // callback
-    const callback = value
-    const options = modifiers || { passive: true }
+    if (typeof window !== 'undefined') {
+      const callback = value
+      const options = modifiers || { passive: true }
 
-    window.addEventListener('resize', callback, options)
-    el._onResize = {
-      callback,
-      options,
-    }
+      window.addEventListener('resize', callback, options)
 
-    if (!modifiers || !modifiers.quiet) {
-      callback()
+      el._onResize = {
+        callback,
+        options,
+      }
+
+      if (!modifiers || !modifiers.quiet) {
+        callback()
+      }
     }
   },
 
   beforeUnmount(el) {
     if (!el._onResize) return
-
-    const { callback, options } = el._onResize
-    window.removeEventListener('resize', callback, options)
-    delete el._onResize
+    if (typeof window !== 'undefined') {
+      const { callback, options } = el._onResize
+      window.removeEventListener('resize', callback, options)
+      delete el._onResize
+    }
   },
 }
